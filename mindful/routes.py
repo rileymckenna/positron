16d0a461ea2e33d90a7e1b0ccfc20ede96eaece6
Jonathan Nguyen <jnguy@microsoft.com>
2020-07-29 16:45:58 -0700

    'stashing'

--- mindful/routes.py
+++ mindful/routes.py
@@ -6,18 +6,15 @@ from mindful.forms import CheckIn
 from mindful.models import User, Mood
 import json
 
-
 @app.route("/")
 @app.route("/home")
 def home():
-    data = Mood.query.all()
-    return render_template('home.html', data = data)
-
+    moods = Mood.query.all()
+    return render_template('home.html', moods=moods)
 
 @app.route("/dev")
 def dev():
     return render_template('credits.html')
-
 
 @app.route("/privacy")
 def privacy():
@@ -44,8 +41,6 @@ def checkin():
             pass
         else:
             pass
-        if(form.text.data != None):
-            post.content = form.text.data
         if post:
             db.session.add(post)
             db.session.commit()
@@ -57,48 +52,13 @@ def checkin():
                            form=form, legend='How are you feeling today?')
 
 
-@app.route("/users/<user>", methods=['GET'])
-def get_user(user):
-    users = User.query.filter_by(teams_id=user).first()
-    if(users == None):
-        return "User not found"
-    else:
-        return render_template('user.html', user=users)
-
-
-@app.route("/users/new", methods=['POST'])
-def create_user():
-    content = request.json
-    tms_id = content['teams_id']
-
-    if(tms_id != None):
-        possible_user = User.query.filter_by(teams_id=tms_id).first()
-        if(possible_user == None):
-            user = User()
-            user.teams_id = tms_id
-            db.session.add(user)
-            db.session.commit()
-            urlRedirect = 'users/' + user.teams_id
-            return redirect(url_for(urlRedirect), 200)
-        else:
-            return redirect(url_for('home'), 404)
-    else:
-        return redirect(url_for('home'), 404)
-
-
-@app.route("/resources")
-def resources():
-    return render_template("resources.html")
-
-
-@app.route("/users/<user>/new", methods=['POST'])
-def createUser(user):
+@app.route("/users/<user>/checkin", methods=['POST'])
+def checkin_post(user):
     content = request.json
 
-    # TODO: Sanitize input
+    #TODO: Sanitize input
     if (0 < content['rating'] and content['rating'] < 4):
-        post = Mood(rating=content['rating'],
-                    user_id=user, content=content['content'])
+        post = Mood(rating = content['rating'], user_id = user, content = content['content'])
     if post:
         db.session.add(post)
         db.session.commit()
@@ -106,3 +66,7 @@ def createUser(user):
     else:
         flash('failed to save response')
     return content
+
+@app.route("/resources")
+def resources():
+    return render_template("resources.html")
