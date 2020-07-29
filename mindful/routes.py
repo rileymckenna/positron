8be3c71394b3ab395a5f7a9b27366f234a054313
Jonathan Nguyen <jnguy@microsoft.com>
2020-07-28 18:19:25 -0700

    added post routes and resources page for redirects

--- mindful/routes.py
+++ mindful/routes.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request, jsonify
 from mindful import db, app
 from flask_login import current_user
 from mindful.forms import CheckIn
@@ -30,7 +30,7 @@ def checkin():
     form = CheckIn()
     if form.validate_on_submit():
         if form.mood0.data == True:
-            post = Mood(rating=0, user_id=1)
+            post = Mood(rating=0, user_id=1, content=content)
             pass
         if(form.mood1.data == True):
             post = Mood(rating=1, user_id=1)
@@ -49,3 +49,23 @@ def checkin():
         return redirect(url_for('home'))
     return render_template('checkin.html', title='Mood',
                            form=form, legend='How are you feeling today?')
+
+
+@app.route("/users/<user>/checkin", methods=['POST'])
+def checkin_post(user_id):
+    content = request.json
+
+    #TODO: Sanitize input
+    if (0 < content.rating and content.rating < 4):
+        post = Mood(rating = content.rating, user_id = content.user_id)
+    if post:
+        db.session.add(post)
+        db.session.commit()
+        flash('Thanks for your feedback', 'success')
+    else:
+        flash('failed to save response')
+    return content
+
+@app.route("/resources")
+def resources():
+    return render_template("resources.html")
